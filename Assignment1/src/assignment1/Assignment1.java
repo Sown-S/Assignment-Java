d5b5c4dca29a3732f26eee7c41477f177412c7d7
Son <160404128+Sown-S@users.noreply.github.com>
2024-08-22 22:23:33 +0700

    Add files via upload

---
+++ Assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,38 @@
+package assignment1;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+//        int numberOfPlayers;
+//        numberOfPlayers=3;
+//        double myGPA;
+//        myGPA= 10 +2.25;
+//        
+//        int legalAge = 18;
+//        int voterAge = legalAge;
+//        
+//        System.out.println("number Of Players = " + numberOfPlayers);
+//        System.out.println("my GPA = " + myGPA);
+//        System.out.println("voter Age = " + voterAge);
+
+//        float a, b, c, d;
+//        a= 14/4;
+//        b= 32/5;
+//        c= 84/10;
+//        d= 156/100;
+//        System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
+//        int a, b;
+//        a = 14 % 4;
+//        b = 218 % 5;
+//        System.out.println(a + "\n" + b);
+
+        int a = 1;
+        int b = a++ + ++a + ++a + a++;
+        System.out.println("a = " + a + "\nb = " + b);
+                
+        
+        
+
+    }
+
+}
